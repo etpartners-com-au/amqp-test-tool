e51ac8378d2c428b41ea6b74bb95cc87e28dbf21
Terry R <Terry.Rankine@etpartners.com.au>
2020-08-14 11:02:26 +0800

    * updated encoding

--- client.py
+++ client.py
@@ -14,6 +14,7 @@ import glob
 import logging
 import time
 import uuid
+import codecs
 
 from proton import Message
 from proton.utils import BlockingConnection
@@ -21,6 +22,13 @@ from proton.utils import BlockingConnection
 servername="test.wencomine.sydney.gacims.net"
 serverport=5672
 creds=("wencoservices", urllib.parse.quote_plus('*****CHANGEME******'))
+
+'''
+utf-8
+utf-16-le
+utf-32
+'''
+myEncoding = 'utf-16-le'
 
 messagePattern="*.xml"
 myFileList = glob.glob(messagePattern, recursive=False)
@@ -34,18 +42,23 @@ logger.setLevel(logging.INFO)
 
 
 conn = BlockingConnection(url="amqp://{}:{}@{}:{}/".format(creds[0],creds[1],servername, serverport))
-sender = conn.create_sender("FMS.Outbound.WorkExecuted")
+sender = conn.create_sender("FMS.Outbound.WorkExecuted.Error")
 
 for file in myFileList:
-    with open(file) as f:
-        myMessage = Message(body=f.read(), 
-                            annotations={"one":1, "two":2}, 
-                            creation_time=time.time(), 
-                            id=uuid.uuid1(), 
-                            correlation_id=uuid.uuid1(),
-                            subject=file)
-
-    sender.send(myMessage, timeout=10);
-    logger.info("sent message: {}".format(file))
+    try:
+        with open(file, 'rb') as f:
+            original=f.read()
+            encodedMessage = codecs.encode(original, encoding=myEncoding)
+            myMessage = Message(body=encodedMessage, 
+                                annotations={"one":1, "two":2}, 
+                                creation_time=time.time(), 
+                                id=uuid.uuid1(), 
+                                correlation_id=uuid.uuid1(),
+                                subject=file)
+    
+        sender.send(myMessage, timeout=10);
+        logger.info("sent message: {}".format(file))
+    except Exception as e:
+        logger.error(e)
 
 conn.close()
